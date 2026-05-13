fbe1296d6a98e8d58c51be5a335e3970c22d0199
mukool <mukulrao202030@gmail.com>
2026-05-13 15:08:50 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,15 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: "https://fypmanagementsystem-production.up.railway.app",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: "https://fypmanagementsystem-production.up.railway.app",
+const api = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: true,
 });
+
+export default api;
